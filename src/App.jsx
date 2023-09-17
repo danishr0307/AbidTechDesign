05f6d63e3b26f52696461c5ebf0388a5c230abea
Danish <draza4299@gmail.com>
2023-09-17 12:15:50 +0530

    New Update fold and sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router";
@@ -12,9 +12,15 @@ import CloseIcon from "./images/CLoseIcon.svg";
 import FullWidthIcon from "./images/fullWidthIcon.svg";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [heightInc, setHeightInc] = useState(false);
+  const checkStatus = () => {
+    setIsOpen(!isOpen);
+    setHeightInc(false);
+  };
   return (
     <>
-      <div className="mx-10">
+      <div className="mx-0">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -23,19 +29,28 @@ function App() {
           <Route path="/work" element={<Work />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <section className="">
-          <div className="absolute bottom-8 right-8">
-            <button className="closeIcon">
+        <section className={isOpen ? "sticky" : "hidden"}>
+          <div className=" fixed bottom-2 right-4">
+            <button className="closeIcon" onClick={checkStatus}>
               <img
-                className="closeIConSize h-14 w-18"
+                className="closeIConSize h-10 w-12"
                 src={CloseIcon}
                 alt="CloseIcon"
               />
             </button>
-            <video autoPlay muted className="videoFix">
-              <source className="" src={abidVideo} type="video/mp4" />
+            <video
+              autoPlay
+              muted={!heightInc}
+              className={heightInc ? "videoHeightInc" : "videoFix"}>
+              <source
+                className=""
+                src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+                type="video/mp4"
+              />
             </video>
-            <button className="fullWidthIcon">
+            <button
+              className="fullWidthIcon"
+              onClick={() => setHeightInc(true)}>
               <img
                 className="widthIconSize h-10 w-8"
                 src={FullWidthIcon}
